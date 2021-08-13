a16a5841772ca18f83e5f408160ded7677f08ae7
Nicholas <nick.c.ruzicka@gmail.com>
2021-08-12 20:03:36 -0400

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.innerHTML = "Nick is the champion";
+
+newHeader.setAttribute("id", "victory");
+
+document.body.append(newHeader);
